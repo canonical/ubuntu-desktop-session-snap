454a964dff51cc5268a05786b186cada67313f45
Ken VanDine <ken.vandine@canonical.com>
2023-06-08 22:41:45 -0400

    Set $HOME to REALHOME and ensure xdg-user-dirs are created and used. This improves the user experience in nautilus and xdg-desktop-portals

--- scripts/desktop-launch.sh
+++ scripts/desktop-launch.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-exec gdbus call \
-     --session \
-     --dest io.snapcraft.Launcher \
-     --object-path /io/snapcraft/PrivilegedDesktopLauncher \
-     --method io.snapcraft.PrivilegedDesktopLauncher.OpenDesktopEntry \
-     "$1"
+# Set $REALHOME to the users real home directory
+REALHOME=$(getent passwd $UID | cut -d ':' -f 6)
+export HOME=$REALHOME
+
+# If xdg-user-dirs-update exists in $PATH, run it
+if command -v xdg-user-dirs-update >/dev/null; then
+  xdg-user-dirs-update
+fi
+
+exec "$@"
